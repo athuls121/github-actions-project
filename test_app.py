36b702bb431af6f64cb9510a79317e979a111e56
athuls121 <athuls121@gmail.com>
2023-11-30 20:07:23 -0500

    deploy v2

--- test_app.py
+++ test_app.py
@@ -1,9 +1,8 @@
 # tests/test_app.py
 
 import unittest
-#from flask import Flask, render_template, request, jsonify
-#import flask
 #from app import app
+import app
 
 class TestApp(unittest.TestCase):
     def setUp(self):
